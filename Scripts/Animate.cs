a7a5458924386057c4e2e57d19f2ea76906adce4
jmickle66666666 <jaymickle@gmail.com>
2016-07-04 15:57:22 +0100

    Rewrite and generalise Animate.cs
    This is untested!! :< but probably works lol iunno

--- Scripts/Animate.cs
+++ Scripts/Animate.cs
@@ -1,89 +1,55 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
 public class Animate : MonoBehaviour {
 
+	// List of animations for the object
+	Dictionary<string, Sprite> animations;
+	// Internal timer
 	float timer = 0;
-
-	
-	List<Sprite> currentAnim;
-	List<Sprite> downAnim;
-	List<Sprite> upAnim;
-	List<Sprite> sideAnim;
-	List<Sprite> downIdle;
-	List<Sprite> upIdle;
-	List<Sprite> sideIdle;
-
-	int dir = 0;
-
+	// Name of currently playing animation
+	string currentAnim;
+	// Sprite Renderer component so it can play animations dummy
 	SpriteRenderer spriteRenderer;
-	
+	// Current frame
 	int spriteIndex = 0;
 
 	// Use this for initialization
 	void Start () {
-
-
-		downAnim = new List<Sprite>();
-		sideAnim = new List<Sprite>();
-		upAnim = new List<Sprite>();
-
-		downIdle = new List<Sprite>();
-		sideIdle = new List<Sprite>();
-		upIdle = new List<Sprite>();
-
-		downAnim.Add(Resources.Load<Sprite>("Sprites/Player/player_down2"));
-		downAnim.Add(Resources.Load<Sprite>("Sprites/Player/player_down3"));
-		downAnim.Add(Resources.Load<Sprite>("Sprites/Player/player_down4"));
-		downAnim.Add(Resources.Load<Sprite>("Sprites/Player/player_down1"));
-
-		upAnim.Add(Resources.Load<Sprite>("Sprites/Player/player_up2"));
-		upAnim.Add(Resources.Load<Sprite>("Sprites/Player/player_up3"));
-		upAnim.Add(Resources.Load<Sprite>("Sprites/Player/player_up4"));
-		upAnim.Add(Resources.Load<Sprite>("Sprites/Player/player_up1"));
-
-		sideAnim.Add(Resources.Load<Sprite>("Sprites/Player/player_side2"));
-		sideAnim.Add(Resources.Load<Sprite>("Sprites/Player/player_side3"));
-		sideAnim.Add(Resources.Load<Sprite>("Sprites/Player/player_side4"));
-		sideAnim.Add(Resources.Load<Sprite>("Sprites/Player/player_side1"));
-
-		sideIdle.Add(Resources.Load<Sprite>("Sprites/Player/player_side1"));
-		downIdle.Add(Resources.Load<Sprite>("Sprites/Player/player_down1"));
-		upIdle.Add(Resources.Load<Sprite>("Sprites/Player/player_up1"));
-
-		currentAnim = downIdle;
-
+		InitializeList();
 		spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 	
+	// Initialize animations list
+	void InitializeList() {
+		if (animations == null) animations = new Dictionary<string, Sprite>();
+	}
+
+	// Adds a sprite frame to an animation, creating the animation if it doesn't already exist
+	void Add (string name, Sprite frame) {
+		if (animations.ContainsKey(name)) {
+			// If the animation exists, add the new frame to the end.
+			animations[name].Add(frame);
+		} else {
+			// If the animation doesn't exist, create a new animation for it.
+			List<Sprite> newAnimation = new List<Sprite>();
+			// Then add the frame
+			newAnimation.Add(frame);
+			animations.Add(name,newAnimation);
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
 		timer += Time.deltaTime * 100;
-		
-		spriteIndex = (int) Mathf.Floor(timer/16) % currentAnim.Count;
-		spriteRenderer.sprite = currentAnim[spriteIndex];
+		spriteIndex = (int) Mathf.Floor(timer/16) % animations[currentAnim].Count;
+		spriteRenderer.sprite = animations[currentAnim][spriteIndex];
+	}
 
-		if (Input.GetAxisRaw("Vertical") > 0) dir = 1;
-		if (Input.GetAxisRaw("Horizontal") > 0) dir = 0;
-		if (Input.GetAxisRaw("Vertical") < 0) dir = 3;
-		if (Input.GetAxisRaw("Horizontal") < 0) dir = 2;
-		if (Input.GetAxisRaw("Horizontal") != 0) spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") < 0;
-
-		bool moving = !(Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0);
-
-		if (moving) {
-			if (dir == 0) currentAnim = sideAnim;
-			if (dir == 1) currentAnim = upAnim;
-			if (dir == 2) currentAnim = sideAnim;
-			if (dir == 3) currentAnim = downAnim;
-		} else {
-			timer = 0;
-			if (dir == 0) currentAnim = sideIdle;
-			if (dir == 1) currentAnim = upIdle;
-			if (dir == 2) currentAnim = sideIdle;
-			if (dir == 3) currentAnim = downIdle;
-			spriteRenderer.sprite = currentAnim[spriteIndex];
-		}
+	// Play a new animations, this resets the frame too.
+	void Play (string animation) {
+		currentAnim = animation;
+		timer = 0;
 	}
 }
