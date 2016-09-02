fc04928c08f69c84b1abde8727054955d274a7eb
jmickle66666666 <jaymickle@gmail.com>
2016-09-02 11:13:32 +0100

    Example

---
+++ Examples/Anim8/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	AnimJtor anim;
+	SpriteRenderer spriteRenderer;
+
+	void Start () {
+		spriteRenderer = GetComponentsInChildren<SpriteRenderer>()[0];
+
+		anim = new AnimJtor("Player",spriteRenderer);
+		anim.Load("Run");
+		anim.Load("Idle");
+		anim.speed = 200.0f;
+	}
+	
+	void Update () {
+		bool moving = !(Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0);
+
+		if (moving) {
+			anim.Play("Run");
+		} else {
+			anim.Play("Idle");
+		}
+
+		anim.Update();
+	}
+}
