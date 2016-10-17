8a940cf780b7e8b4b8bb692c42573533f66d199e
jmickle66666666 <jaymickle@gmail.com>
2016-10-17 12:32:50 +0100

    Add TextureImport
    For importing textures and creating materials based on a selected
    prefab. UNTESTED

---
+++ Scripts/TextureImport.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class TextureImport : AssetPostprocessor {
+
+    public Material templateMaterial;
+
+	void OnPreprocessTexture() {
+        // if we dumped a png in the _texture folder...
+        if (assetPath.Contains("_texture")) {
+            
+            // set up the texture defaults
+            TextureImporter textureImporter = (TextureImporter) assetImporter;
+            textureImporter.filterMode = FilterMode.Point;
+            textureImporter.wrapMode = TextureWrapMode.Repeat;
+            textureImporter.textureType = TextureImporterType.Image;
+            
+            // parse out the name of the texture
+            int a = assetPath.LastIndexOf('/')+1;
+            int b = assetPath.LastIndexOf('.');
+            string name = assetPath.Substring(a,b-a);
+            
+            AssetDatabase.Refresh();
+            
+            // create a material for it
+            Material material = new Material(templateMaterial);
+            Texture2D t = (Texture2D)AssetDatabase.LoadAssetAtPath(assetPath, typeof(Texture2D));
+            material.SetTexture("_MainTex",t);
+            AssetDatabase.CreateAsset(material, "Assets/Materials/"+name+".mat");
+        }
+    }
+}
